8bd034021af2389cdd6e0b4a03b271bbb42e2e44
hari ushasri <hariusha8106@gmail.com>
2024-04-30 16:39:19 +0000

    solution

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+//   Write your code here.
+
+<div style="text-align: center;">
+<img src="https://assets.ccbp.in/frontend/content/react-js/super-over-league-lg-output.png" alt="supe-over-league-output" style="max-width:70%;box-shadow:0 2.8px 2.2px rgba(0, 0, 0, 0.12)">
+</div>
+<br/>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
